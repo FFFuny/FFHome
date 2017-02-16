7921f03a401034ddf6087b0bdaeacafa53a7a349
FFFuny <liufei92@126.com>
2017-02-16 16:56:32 +0800

    加入media
    media

---
+++ FFHome/FFHome/Class/Media/C/MediaViewController.h
@@ -0,0 +1,13 @@
+//
+//  MediaViewController.h
+//  FFHome
+//
+//  Created by 建新 on 17/2/16.
+//  Copyright © 2017年 ff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaViewController : UIViewController
+
+@end
--- FFHome/FFHome/ViewController.m
+++ FFHome/FFHome/ViewController.m
@@ -9,11 +9,13 @@
 #import "ViewController.h"
 #import "HomeViewController.h"
 #import "MineViewController.h"
+#import "MediaViewController.h"
 
 @interface ViewController ()<UIGestureRecognizerDelegate>
 {
     HomeViewController *home;
     MineViewController *mine;
+    MediaViewController *media;
 }
 
 @property (nonatomic, assign) CGPoint beginLocation;
@@ -28,51 +30,55 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     BOOL isFinger = [[NSUserDefaults standardUserDefaults] boolForKey:@"isFinger"];
-    if (isFinger) {
-        
-        LAContext *ctx = [[LAContext alloc] init];
-        if ([ctx canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:NULL]) {
-            
-            // 识别代码部分
-            [ctx evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"通过Home键验证已有手机指纹" reply:^(BOOL success, NSError * _Nullable error) {
-                
-                if (success) {
-                    
-                    NSLog(@"成功");
-                    home = [[HomeViewController alloc] init];
-                    [self.view addSubview:home.view];
-                    
-                    mine = [[MineViewController alloc] init];
-                    [self.view addSubview:mine.view];
-                    
-                    [self.view bringSubviewToFront:home.view];
-                    
-                    UILongPressGestureRecognizer *gesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(popPininterestMenu:)];
-                    gesture.delegate = self;
-                    [self.view addGestureRecognizer:gesture];
-
-                } else {
-                    
-                    NSLog(@"取消");
-                    if (error.code == LAErrorUserFallback) {
-                        
-                        dispatch_async(dispatch_get_main_queue(), ^{
-                            
-                            // 密码验证
-                        });
-                    } else if (error.code == LAErrorUserCancel) {
-                        
-                        NSLog(@"取消2");
-                        [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"isFinger"];
-                    }
-                }
-            }];
-        }
-    } else {
-        
+    
+//    if (isFinger) {
+//        
+//        LAContext *ctx = [[LAContext alloc] init];
+//        if ([ctx canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:NULL]) {
+//            
+//            // 识别代码部分
+//            [ctx evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"通过Home键验证已有手机指纹" reply:^(BOOL success, NSError * _Nullable error) {
+//                
+//                if (success) {
+//                    
+//                    NSLog(@"成功");
+//                    home = [[HomeViewController alloc] init];
+//                    [self.view addSubview:home.view];
+//                    
+//                    mine = [[MineViewController alloc] init];
+//                    [self.view addSubview:mine.view];
+//                    
+//                    [self.view bringSubviewToFront:home.view];
+//                    
+//                    UILongPressGestureRecognizer *gesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(popPininterestMenu:)];
+//                    gesture.delegate = self;
+//                    [self.view addGestureRecognizer:gesture];
+//
+//                } else {
+//                    
+//                    NSLog(@"取消");
+//                    if (error.code == LAErrorUserFallback) {
+//                        
+//                        dispatch_async(dispatch_get_main_queue(), ^{
+//                            
+//                            // 密码验证
+//                        });
+//                    } else if (error.code == LAErrorUserCancel) {
+//                        
+//                        NSLog(@"取消2");
+//                        [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"isFinger"];
+//                    }
+//                }
+//            }];
+//        }
+//    } else {
+    
         home = [[HomeViewController alloc] init];
         [self.view addSubview:home.view];
-        
+    
+        media = [[MediaViewController alloc] init];
+        [self.view addSubview:media.view];
+    
         mine = [[MineViewController alloc] init];
         [self.view addSubview:mine.view];
         
@@ -82,7 +88,7 @@
         gesture.delegate = self;
         [self.view addGestureRecognizer:gesture];
 
-    }
+//    }
     
 }
 
@@ -101,10 +107,17 @@
         PininterestLikeMenuItem *item1 = [[PininterestLikeMenuItem alloc] initWithImage:[UIImage imageNamed:@"center"]
                                                                            selctedImage:[UIImage imageNamed:@"center-highlighted"]
                                                                           selectedBlock:^(void) {
+                                                                              [self.view bringSubviewToFront:media.view];            NSLog(@"item 1 selected");
+                                                                          }];
+
+        PininterestLikeMenuItem *item2 = [[PininterestLikeMenuItem alloc] initWithImage:[UIImage imageNamed:@"center"]
+                                                                           selctedImage:[UIImage imageNamed:@"center-highlighted"]
+                                                                          selectedBlock:^(void) {
                                                                               [self.view bringSubviewToFront:mine.view];            NSLog(@"item 1 selected");
                                                                           }];
+
         
-        NSArray *subMenus = @[item0, item1];
+        NSArray *subMenus = @[item0, item1, item2];
         
         self.menu = [[PininterestLikeMenu alloc] initWithSubMenus:subMenus withStartPoint:self.beginLocation];
     }
